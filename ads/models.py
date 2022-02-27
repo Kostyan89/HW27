8e8998ef5ad29dbaeba710d7f8e6bfe5ce5da64d
Kostyan89 <kostasamin@gmail.com>
2022-02-27 21:11:50 +0200

    add def __str__

--- ads/models.py
+++ ads/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 
 class Ad(models.Model):
     name = models.CharField(max_length=200)
@@ -12,3 +15,6 @@ class Ad(models.Model):
     description = models.TextField(null=True)
     address = models.CharField(max_length=255)
     is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
