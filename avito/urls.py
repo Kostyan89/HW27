7db9aaad7e40d00d2dbec5ea4bc12c0e8fe1ba1f
Kostyan89 <kostasamin@gmail.com>
2022-03-05 18:47:51 +0200

    add urls for categories

--- avito/urls.py
+++ avito/urls.py
@@ -20,8 +20,11 @@ from ads import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cat/', views.CategoryView.as_view()),
+    path('cat/', views.CategoryListView.as_view()),
+    path('cat/create/', views.CategoryCreateView.as_view()),
     path('cat/<int:pk>', views.CategoryDetailView.as_view()),
+    path('cat/<int:pk>/update/', views.CategoryUpdateView.as_view()),
+    path('cat/<int:pk>/delete/', views.CategoryDeleteView.as_view()),
     path('ad/', views.AdListView.as_view()),
     path('ad/create/', views.AdCreateView.as_view()),
     path('ad/<int:pk>', views.AdDetailView.as_view()),
