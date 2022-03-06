f8a4aab38f86783d384f3ba8b8d9ba55023697be
Kostyan89 <kostasamin@gmail.com>
2022-03-06 21:20:04 +0200

    some fix

--- users/urls_users.py
+++ users/urls_users.py
@@ -2,9 +2,9 @@ from django.urls import path
 from ads import views
 
 urlpatterns = [
-    # path('', views.UserListView.as_view()),
-    # path('create/', views.UserCreateView.as_view()),
-    # path('<int:pk>/', views.UserDetailView.as_view()),
-    # path('<int:pk>/update/', views.UserUpdateView.as_view()),
-    # path('<int:pk>/delete/', views.UserDeleteView.as_view()),
+    path('', views.UserListView.as_view()),
+    path('create/', views.UserCreateView.as_view()),
+    path('<int:pk>/', views.UserDetailView.as_view()),
+    path('<int:pk>/update/', views.UserUpdateView.as_view()),
+    path('<int:pk>/delete/', views.UserDeleteView.as_view()),
     ]
