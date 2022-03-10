ab13de6a75f898c1db9607a75a436b2727f2e0d5
Kostyan89 <kostasamin@gmail.com>
2022-03-10 11:20:38 +0200

    some сommit

--- users/models.py
+++ users/models.py
@@ -29,7 +29,7 @@ class User(models.Model):
     password = models.CharField(max_length=128)
     role = models.CharField(max_length=9, choices=ROLES, default="member")
     age = models.PositiveIntegerField()
-    location = models.ManyToManyField(Location)
+    locations = models.ManyToManyField(Location)
 
     class Meta:
         verbose_name = "Пользователь"
