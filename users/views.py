9847b87472779204ebd217c1a3b20d06127cde0d
Kostyan89 <kostasamin@gmail.com>
2022-03-17 19:33:50 +0200

    some changes

--- users/views.py
+++ users/views.py
@@ -1,15 +1,5 @@
-import json
-
-from django.core.paginator import Paginator
-from django.db.models import Count
-from django.http import JsonResponse
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
-
-from avito import settings
-from users.models import User, Location
+from users.models import User
 from users.serializers import UserRetrieveSerializer, UserCreateSerializer, UserListSerializer, UserDeleteSerializer
 
 
