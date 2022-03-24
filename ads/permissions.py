29cb0391aa3fe26dd3c80235dcfbfb59d617fafa
Kostyan89 <kostasamin@gmail.com>
2022-03-24 20:43:59 +0200

    some commit

--- ads/permissions.py
+++ ads/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import BasePermission
 
+from ads.models import Compilation
 from users.models import User
 
 
@@ -8,3 +9,29 @@ class AdCreatePermission(BasePermission):
 
     def has_permission(self, request, view):
         return request.user.role == User.HR
+
+
+class CompilationUpdatePermission(BasePermission):
+    message = 'You can not manage not your compilations'
+
+    def has_permission(self, request, view):
+        entity = Compilation.objects.get(pk=view.kwargs["pk"])
+        if entity.owner_id == request.user.id:
+            return True
+        return False
+
+
+class IsAuthenticatedAndOwner(BasePermission):
+    message = 'You must be the owner of this object.'
+
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
+
+
+class AdUpdatePermission(BasePermission):
+    message = 'Managing others ads not permitted.'
+
+    def has_permission(self, request, view):
+        if request.user.role in [User.HR, User.EMPLOYEE]:
+            return True
+        return False
--- ads/serializers.py
+++ ads/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from ads.models import Ad
+from ads.models import Ad, Compilation
 
 
 class AdListSerializer(serializers.ModelSerializer):
@@ -19,6 +19,27 @@ class AdSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ad
         fields = '__all__'
+
+
+class CompilationListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Compilation
+        fields = ["id", "name"]
+
+
+class CompilationDetailSerializer(serializers.ModelSerializer):
+    items = AdListSerializer(many=True)
+
+    class Meta:
+        model = Compilation
+        fields = '__all__'
+
+
+class CompilationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Compilation
+        fields = '__all__'
+
 
 
         
