a6fb6fe2eb2b079d4528fe429a9daeef9c2a6ed3
Kostyan89 <kostasamin@gmail.com>
2022-04-06 17:41:54 +0300

    some commit

---
+++ tests/Ad/ad_create_test.py
@@ -0,0 +1,39 @@
+import pytest
+
+from users.models import User
+
+
+@pytest.mark.django_db
+def test_create_ad(client, hr_token):
+    access_token, refresh_token = hr_token
+    expected_response = {
+        "id": 1,
+        "name": "test",
+        "price": 1,
+        "is_published": "False",
+        "author": User,
+        "description": "test",
+        "address": "test",
+        "category": None,
+        "image": None,
+    }
+
+    data = {
+        "name": "test",
+        "price": 1,
+        "is_published": "False",
+        "description": "test",
+        "address": "test",
+    }
+
+    response = client.post(
+        "/ad/create/",
+        data,
+        content_type="application/json",
+        HTTP_AUTHORIZATION="Bearer " + access_token
+    )
+
+    assert response.status_code == 201
+    assert response.data == expected_response
+
+
---
+++ tests/factory.py
@@ -0,0 +1,23 @@
+import factory
+from ads.models import Ad
+from users.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = "test"
+    password = "123qwe"
+
+
+class AdFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Ad
+
+    name = "test"
+    price = 1
+    address = "test"
+    author = factory.SubFactory(UserFactory)
+    description = "test"
+    is_published = "False"
